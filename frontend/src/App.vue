4d863350902b225ea2095b909ca46321bbccb1ed
Max Henkel <max@maxhenkel.de>
2021-08-09 18:35:55 +0200

    Add privacy and legal

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,15 +2,17 @@
   <v-app>
     <v-app-bar app color="gray" dark>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Weather</span>
+        <router-link to="/" class="no-link"><span>WEATHER</span></router-link>
       </v-toolbar-title>
 
       <v-spacer></v-spacer>
 
+      <v-btn to="/privacy" plain>Privacy</v-btn>
+      <v-btn to="/legal" plain>Legal</v-btn>
       <v-btn
         href="https://github.com/henkelmax/weather-server"
         target="_blank"
-        text
+        plain
       >
         <span class="mr-2">Source Code</span>
         <v-icon>mdi-open-in-new</v-icon>
@@ -28,3 +30,10 @@ export default {
   name: "App",
 };
 </script>
+
+<style scoped>
+.no-link {
+  color: white;
+  text-decoration: none;
+}
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Weather from '../views/Weather.vue'
+import Privacy from '../views/Privacy.vue'
+import Legal from '../views/Legal.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +11,16 @@ const routes = [
     path: '/',
     name: 'Weather',
     component: Weather
+  },
+  {
+    path: '/privacy',
+    name: 'Privacy',
+    component: Privacy
+  },
+  {
+    path: '/legal',
+    name: 'Legal',
+    component: Legal
   }
 ]
 
