e1e87d3d8e4f16e91a4b6fb95de241d25904435e
Max Henkel <max@maxhenkel.de>
2024-04-04 14:22:38 +0200

    Fix import

--- backend/src/main/java/de/maxhenkel/weatherserver/controllers/ImportController.java
+++ backend/src/main/java/de/maxhenkel/weatherserver/controllers/ImportController.java
@@ -2,14 +2,13 @@ package de.maxhenkel.weatherserver.controllers;
 
 import de.maxhenkel.weatherserver.annotations.ValidateApiKey;
 import de.maxhenkel.weatherserver.csvimport.ImportService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
@@ -21,11 +20,11 @@ public class ImportController {
     @Autowired
     private ImportService importService;
 
-    @PostMapping
+    @PostMapping(consumes = "text/csv")
     @ValidateApiKey
-    public ResponseEntity<String> importData(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<String> importData(HttpServletRequest request) {
         try {
-            int entryCount = importService.importCsv(file.getInputStream());
+            int entryCount = importService.importCsv(request.getInputStream());
             return ResponseEntity.ok().body("Imported %s entries".formatted(entryCount));
         } catch (IOException e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "File could not be read");
