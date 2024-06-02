5ed1c539e69da7f3c9064dc052b21c4c0ad3e2eb
Max Henkel <max@maxhenkel.de>
2024-06-02 22:18:23 +0200

    Allow CORS for svg widget

--- backend/src/main/java/de/maxhenkel/weatherserver/controllers/SvgController.java
+++ backend/src/main/java/de/maxhenkel/weatherserver/controllers/SvgController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 
+@CrossOrigin
 @RestController
 public class SvgController {
 
