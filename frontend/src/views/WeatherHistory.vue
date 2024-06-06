d2f6101cc4c30b844fdce57e5edbf7245a828573
Max Henkel <max@maxhenkel.de>
2024-06-06 10:23:52 +0200

    Rework date picker

--- frontend/src/views/WeatherHistory.vue
+++ frontend/src/views/WeatherHistory.vue
@@ -1,21 +1,15 @@
 <template>
   <v-container class="weather-container">
     <v-row class="text-center" justify="center">
-      <v-col cols="12" class="pa-1">
+      <v-col cols="12" lg="3" md="4" class="pa-1">
+        <StationCard :station="station"/>
+      </v-col>
+    </v-row>
+    <v-row class="text-center" justify="center">
+      <v-col cols="12" lg="3" md="4" class="pa-1">
         <v-card>
-          <v-card-text>
-            <v-menu v-model="dateMenu" location="bottom" :close-on-content-click="false">
-              <template v-slot:activator="{ props }">
-                <v-text-field
-                    v-model="formattedDate"
-                    label="Date"
-                    prepend-icon="mdi-calendar"
-                    readonly
-                    v-bind="props"
-                ></v-text-field>
-              </template>
-              <v-date-picker v-model="date" :allowed-dates="allowedDates"></v-date-picker>
-            </v-menu>
+          <v-card-text class="d-flex justify-center">
+            <v-date-picker v-model="date" :allowed-dates="allowedDates" hide-header></v-date-picker>
           </v-card-text>
         </v-card>
       </v-col>
@@ -27,32 +21,21 @@
 <script setup lang="ts">
 import Graphs from "@/components/weather/Graphs.vue";
 import moment from "moment/min/moment-with-locales";
-import {computed, ref, watch} from "vue";
+import {ref, watch} from "vue";
 import {fetchStation, fetchWeather, getWeatherParams} from "@/utils/api";
 import {useSettingsStore} from "@/stores/settings";
 import {useI18n} from "vue-i18n";
 import {on} from "@/events/eventBus";
+import StationCard from "@/components/weather/StationCard.vue";
 
 const {locale} = useI18n();
 moment.locale(locale.value);
 
 const settingsStore = useSettingsStore();
 
-const date = ref(new Date());
-const dateMenu = ref(false);
+const date = ref(moment().subtract(1, "day").toDate());
 const station = ref<Station | null>(null);
 const weather = ref<Weather[] | null>(null);
-
-const formattedDate = computed(() => {
-  return moment(date.value).format("YYYY-MM-DD");
-});
-
-watch(date, () => {
-  updateWeatherData();
-  setTimeout(() => {
-    dateMenu.value = false;
-  }, 500);
-});
 
 function updateWeatherData() {
   if (station.value == null) {
@@ -69,6 +52,7 @@ function allowedDates(date: unknown): boolean {
 }
 
 on("update", updateWeatherData);
+watch(date, updateWeatherData);
 updateWeatherData();
 </script>
 
